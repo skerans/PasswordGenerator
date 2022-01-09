f72e8da6a1a5f31c8e2374c2373da3ecf92f9b72
Spencer Kerans <skerans0@gmail.com>
2022-01-09 11:58:01 -0700

    js code for no characters selected

--- script.js
+++ script.js
@@ -11,75 +11,83 @@ let pwLength;
 
 // Write password to the #password input
 function writePassword() {
-  
- 
- //Set password length, looping if not within parameters
- while (true) {
-  pwLength =parseInt(prompt("Password length?", "Must be between 8 and 128 characters"));
-  if (pwLength >= 8 && pwLength <= 128) {
-    break;
+
+  pwChars = "";
+  //Set password length, looping if not within parameters
+  while (true) {
+    pwLength = parseInt(prompt("Password length?", "Must be between 8 and 128 characters"));
+    if (pwLength >= 8 && pwLength <= 128) {
+      break;
+    }
+    alert("Password must be between 8 and 128 characters");
   }
-  alert("Password must be between 8 and 128 characters");
-}
-alert("thank you");
+  alert("thank you");
 
-//Set lowercase characters or not
-if (confirm("Use lower case?")) {
-  pwChars += charLower;
-  alert("lowercase added");
-  console.log(pwChars);
-} else {
-  alert("no lower case characters")
-  console.log(pwChars);
-}
-
-//Set uppercase characters or not
-if (confirm("Use uppercase?")) {
-  pwChars += charUpper;
-  alert("uppercase added");
-  console.log(pwChars);
-} else {
-  alert("no upper case characters")
-  console.log(pwChars);
-}
-
-//Set symbols characters or not
-if (confirm("Use symbols?")) {
-pwChars += charSymbol;
-alert("symbols added");
-console.log(pwChars);
-} else {
-alert("no symbol characters")
-console.log(pwChars);
-}
-
-//Set number characters or not
-if (confirm("Use numbers?")) {
-pwChars += charNum;
-alert("numbers added");
-console.log(pwChars);
-} else {
-alert("no number characters")
-console.log(pwChars);
-}
-
-
-let generatePassword = (pwLength, pwChars) => {
-  let pw = "";
-  for (let i = 0; i <= pwLength; i++) {
-    pw += pwChars.charAt(
-      Math.floor(Math.random() * pwChars.length)
-    );
+  //Set lowercase characters or not
+  if (confirm("Use lower case?")) {
+    pwChars += charLower;
+    alert("lowercase added");
+    console.log(pwChars);
+  } else {
+    alert("no lower case characters")
+    console.log(pwChars);
   }
-  return pw;
+
+  //Set uppercase characters or not
+  if (confirm("Use uppercase?")) {
+    pwChars += charUpper;
+    alert("uppercase added");
+    console.log(pwChars);
+  } else {
+    alert("no upper case characters")
+    console.log(pwChars);
+  }
+
+  //Set symbols characters or not
+  if (confirm("Use symbols?")) {
+    pwChars += charSymbol;
+    alert("symbols added");
+    console.log(pwChars);
+  } else {
+    alert("no symbol characters")
+    console.log(pwChars);
+  }
+
+  //Set number characters or not
+  if (confirm("Use numbers?")) {
+    pwChars += charNum;
+    alert("numbers added");
+    console.log(pwChars);
+  } else {
+    alert("no number characters")
+    console.log(pwChars);
+  }
+
+
+  let generatePassword = (pwLength, pwChars) => {
+
+    let pw = "";
+    for (let i = 0; i <= pwLength; i++) {
+      pw += pwChars.charAt(
+        Math.floor(Math.random() * pwChars.length)
+      );
+    }
+    return pw;
   };
 
-var password = generatePassword(pwLength, pwChars);
-     
-var passwordText = document.querySelector("#password");
 
-passwordText.value = password;
 
+  var passwordText = document.querySelector("#password");
+
+
+
+  if (pwChars.length == 0) {
+    passwordText.value = "No choices were selected";
+
+  } else {
+    var password = generatePassword(pwLength, pwChars);
+    passwordText.value = password;
+  }
 }
 
 
